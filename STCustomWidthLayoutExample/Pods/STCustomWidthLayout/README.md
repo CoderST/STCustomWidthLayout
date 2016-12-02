c77011606333ada3a5c8b4ec57583f2423581963
shengtao <694468528@qq.com>
2016-12-02 16:50:52 +0800

    去除打印数据

---
+++ STCustomWidthLayoutExample/Pods/STCustomWidthLayout/README.md
@@ -0,0 +1,2 @@
+# STCustomWidthLayout
+一个自定义宽度的流水布局
--- STCustomWidthLayoutExample/STCustomWidthLayoutExample/ViewController.swift
+++ STCustomWidthLayoutExample/STCustomWidthLayoutExample/ViewController.swift
@@ -7,75 +7,75 @@
 //
 
 import UIKit
+import STCustomWidthLayout
 class ViewController: UIViewController {
 
     // MARK:- 懒加载
     private lazy var array : [TestModel] = [TestModel]()
-//    private lazy var collectionView : UICollectionView = {[weak self] in
-//        let layout = STCustomWidthLayout()
-////        layout.delegate = self
-//        let collectionView = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: layout)
-//        collectionView.backgroundColor = UIColor.orangeColor()
-//        collectionView.dataSource = self
-//        
-//        
-//        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-//        
-//        return collectionView
-//        }()
+    private lazy var collectionView : UICollectionView = {[weak self] in
+        let layout = STCustomWidthLayout()
+        layout.delegate = self
+        let collectionView = UICollectionView(frame: (self?.view.bounds)!, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.orangeColor()
+        collectionView.dataSource = self
+        
+        collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        return collectionView
+        }()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.addSubview(collectionView)
-//        let button = UIButton(type: UIButtonType.ContactAdd)
-//        button.center = CGPoint(x: 300, y: 100)
-//        button.addTarget(self, action: "buttonClick", forControlEvents: .TouchUpInside)
-//        view.addSubview(button)
+        view.addSubview(collectionView)
+        let button = UIButton(type: UIButtonType.ContactAdd)
+        button.center = CGPoint(x: 300, y: 100)
+        button.addTarget(self, action: "buttonClick", forControlEvents: .TouchUpInside)
+        view.addSubview(button)
 
         
     }
     
     // MARK:- 点击事件
-//    func buttonClick(){
-//        let model = TestModel(width : CGFloat(arc4random_uniform(50) + 30),height : 50)
-//        
-//        array.append(model)
-//        
-//        collectionView.reloadData()
-//    }
+    func buttonClick(){
+        let model = TestModel(width : CGFloat(arc4random_uniform(50) + 30),height : 50)
+        
+        array.append(model)
+        
+        collectionView.reloadData()
+    }
     
 
 
 }
 // MARK:- UICollectionViewDataSource
-//extension ViewController : UICollectionViewDataSource {
-//    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-//        
-//        return array.count
-//    }
-//    
-//    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
-//        
-//        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
-//        
-//        cell.backgroundColor = UIColor.redColor()
-//        
-//        return cell
-//    }
-//    
-//}
-//
+extension ViewController : UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        print(array.count)
+        return array.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("cell", forIndexPath: indexPath)
+        
+        cell.backgroundColor = UIColor.redColor()
+        
+        return cell
+    }
+    
+}
+
 // MARK:- STRowFollowLayoutDelegate
-//extension ViewController : STCustomWidthLayoutDelegate {
-//    func stLayoutWidthtForRowAtIndexPath(stLayout: STCustomWidthLayout, indexPath: NSIndexPath) -> CGFloat {
-//        let model = array[indexPath.item]
-//        return model.width
-//    }
-//    
-//    func heightForRowAtIndexPath(stLayout: STCustomWidthLayout) -> CGFloat {
-//        
-//        return 40
-//    }
-//    
-//}
+extension ViewController : STCustomWidthLayoutDelegate {
+    func stLayoutWidthtForRowAtIndexPath(stLayout: STCustomWidthLayout, indexPath: NSIndexPath) -> CGFloat {
+        let model = array[indexPath.item]
+        return model.width
+    }
+    
+    func heightForRowAtIndexPath(stLayout: STCustomWidthLayout) -> CGFloat {
+        
+        return 40
+    }
+    
+}
